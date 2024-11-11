8868fea636ff5ea87702e15a5d01c745381db393
Jertroll <jeremyblanco20@gmail.com>
2024-11-11 02:03:11 -0600

    boton registrar y creacion de registro  usuario clliente

--- reservaParque/src/app/components/login/login.component.ts
+++ reservaParque/src/app/components/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../../services/login.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
